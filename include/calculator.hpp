e6210084b5759a126ba2364921d40ff431250f87
DeKKeRGOD <tigrenvova@mail.ru>
2016-12-28 16:12:45 +0300

    Delete calculator.hpp

--- include/calculator.hpp
+++
@@ -1,6 +0,0 @@
-double sum ( float a,  float b);
-double sub ( float a,  float b);
-double mult ( float a,  float b);
-double *div ( float a,  float b, int* c );
-double pow ( float a, int* extent, int* c);
-double *sq ( float a, int* c );
